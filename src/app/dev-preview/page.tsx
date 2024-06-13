97be9e2a8d675950fbcef3b11abd84b1d645c5be
striker1826 <alstjq123579@daum.net>
2024-06-13 09:57:25 +0900

    프로젝트 초기셋팅

---
+++ src/app/dev-preview/page.tsx
@@ -0,0 +1,5 @@
+const page = () => {
+  return <div>dev-preview</div>;
+};
+
+export default page;
---
+++ src/app/interview/page.tsx
@@ -0,0 +1,5 @@
+const page = () => {
+  return <div>interview page</div>;
+};
+
+export default page;
