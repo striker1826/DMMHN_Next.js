cc5725c8c1067ac54044fb6f7ec5fd451e731ebb
striker1826 <alstjq123579@daum.net>
2024-06-18 15:20:50 +0900

    test: vercel dev브랜치로 배포 되는지 테스트하는 커밋

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,8 +4,6 @@ import { NextRequest, NextResponse } from "next/server";
 export function middleware(req: NextRequest) {
   const userAgent = req.headers.get("user-agent") || "";
   const isMobile = /android|iphone|ipad|ipod|blackberry|iemobile|opera mini|mobile/i.test(userAgent);
-  console.log(userAgent);
-  console.log(isMobile);
 
   if (isMobile) {
     return NextResponse.redirect(new URL("/mobile-page", req.url));
