ce5ff63d7b504c71fcb6f242cab415cfaceb0941
striker1826 <alstjq123579@daum.net>
2024-06-23 12:51:20 +0900

    refactor: simulation 페이지 로직 분리

--- src/components/interview/Start/index.tsx
+++ src/components/interview/Start/index.tsx
@@ -3,86 +3,30 @@ import { useRef, useState } from 'react';
 import { useVideoHandler } from '@/models/simulation/video';
 import { useQuery } from '@tanstack/react-query';
 import { questionApi } from '@/api/question/questionApi';
+import { useHandleInterview } from '@/models/simulation/useHandleInterview';
 
 interface Props {
-  onChangeStatus: (status: 'ready' | 'start' | 'end') => void;
+  handleInterviewStatus: (status: 'ready' | 'start' | 'end') => void;
 }
 
-export const Start = ({ onChangeStatus }: Props) => {
+export const Start = ({ handleInterviewStatus }: Props) => {
   const videoRef = useRef<HTMLVideoElement>(null);
   const { handleStartRecording, handleStopRecording, recording, handleDownload } =
     useVideoHandler(videoRef);
   const { data: questionList } = useQuery(questionApi.queryOptions());
 
-  const [interview, setInterview] = useState<{ isStart: boolean; isEnd: boolean }>({
-    isStart: false,
-    isEnd: false,
-  });
-
-  const [currentQuestion, setCurrentQuestion] = useState<{
-    totalCount: number;
-    currentNumber: number;
-    question: string;
-  }>({
-    totalCount: 500,
-    currentNumber: 0,
-    question: '면접 시작 전입니다',
-  });
-
-  const handleLoadQuestionSetting = () => {
-    if (!questionList) return;
-    if (!questionList[0].question) {
-      alert('준비된 질문이 없습니다.');
-      onChangeStatus('end');
-    }
-
-    setCurrentQuestion(prev => {
-      return {
-        ...prev,
-        totalCount: questionList?.length,
-        currentNumber: currentQuestion.currentNumber + 1,
-        question: questionList[currentQuestion.currentNumber].question,
-      };
-    });
-  };
-
-  const handleStartInterview = () => {
-    handleLoadQuestionSetting();
-    handleStartRecording();
-    setInterview(prev => {
-      return {
-        ...prev,
-        isStart: true,
-      };
-    });
-  };
-
-  const handleLoadNextQuestion = () => {
-    if (!questionList) return;
-
-    setCurrentQuestion(prev => {
-      return {
-        ...prev,
-        currentNumber: prev.currentNumber + 1,
-        question: questionList[currentQuestion.currentNumber].question,
-      };
-    });
-  };
-
-  const handleEndInterview = () => {
-    handleStopRecording();
-    setInterview(prev => {
-      return {
-        ...prev,
-        isEnd: true,
-      };
-    });
-  };
-
-  const handleQuitInterview = () => {
-    handleDownload();
-    onChangeStatus('end');
-  };
+  const {
+    interview,
+    currentQuestion,
+    startInterview,
+    loadNextQuestion,
+    endInterview,
+    quitInterview,
+  } = useHandleInterview(
+    handleInterviewStatus,
+    { handleStartRecording, handleStopRecording, handleDownload },
+    questionList,
+  );
 
   return (
     <>
@@ -101,7 +45,7 @@ export const Start = ({ onChangeStatus }: Props) => {
 
       <button
         className={interview.isStart ? styles.next_btn_none : styles.next_btn}
-        onClick={handleStartInterview}
+        onClick={startInterview}
       >
         면접 시작하기
       </button>
@@ -111,7 +55,7 @@ export const Start = ({ onChangeStatus }: Props) => {
             ? styles.next_btn_none
             : styles.next_btn
         }
-        onClick={handleLoadNextQuestion}
+        onClick={loadNextQuestion}
       >
         다음 질문으로 &gt;
       </button>
@@ -119,13 +63,13 @@ export const Start = ({ onChangeStatus }: Props) => {
       {currentQuestion.currentNumber === currentQuestion.totalCount && (
         <button
           className={interview.isEnd ? styles.next_btn_none : styles.next_btn}
-          onClick={handleEndInterview}
+          onClick={endInterview}
         >
           녹화 중료하기
         </button>
       )}
       {interview.isEnd && (
-        <button className={styles.next_btn} onClick={handleQuitInterview}>
+        <button className={styles.next_btn} onClick={quitInterview}>
           면접 종료하기
         </button>
       )}
