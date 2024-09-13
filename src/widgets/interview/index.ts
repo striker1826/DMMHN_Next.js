18776bbfb3115f210dc111cb8f28540ef3480588
kuum97 <kuum97@icloud.com>
2024-09-13 22:31:27 +0900

    remove: 레이어 변경에 따라 export 경로 수정

--- src/widgets/interview/index.ts
+++ src/widgets/interview/index.ts
@@ -1,3 +1,2 @@
 export { Simulation } from '@/widgets/interview/Simulation';
 export { Stacks } from '@/widgets/interview/Stacks';
-export { StackTypeList } from '@/widgets/interview/StackTypeList';
