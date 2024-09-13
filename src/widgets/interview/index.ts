51dcce76b5d74821ad4efecd28cdf317f89746e2
kuum97 <kuum97@icloud.com>
2024-09-13 15:41:37 +0900

    feat: 스택 타입 리스트 컴포넌트 분리 구현

--- src/widgets/interview/index.ts
+++ src/widgets/interview/index.ts
@@ -1,2 +1,3 @@
 export { Simulation } from '@/widgets/interview/Simulation';
 export { Stacks } from '@/widgets/interview/Stacks';
+export { StackTypeList } from '@/widgets/interview/StackTypeList';
