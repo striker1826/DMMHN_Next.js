5c495c32766ee407dd8d18dde9f23084c57b031c
kuum97 <kuum97@icloud.com>
2024-09-18 19:32:39 +0900

    feat: 인터뷰 페이지 서버 컴포넌트로 전환위해 시뮬레이션 컴포넌트 분리

---
+++ src/app/interview/Simulation.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import styles from './page.module.scss';
+import { useRouter } from 'next/navigation';
+import { useUserInfo } from '@/queries/user/userApi';
+import { End, Ready, Start } from '@/widgets/interview';
+
+const Simulation = ({}) => {
+  const router = useRouter();
+  const [status, setStatus] = useState<'ready' | 'start' | 'end'>('ready');
+  const { data: isLogin, isFetched } = useUserInfo();
+
+  useEffect(() => {
+    if (isFetched && !isLogin) {
+      router.push('/signin');
+    }
+  }, [isFetched, isLogin, router]);
+
+  let content = <Ready onChangeStatus={setStatus} />;
+
+  if (status === 'start') {
+    content = <Start handleInterviewStatus={setStatus} />;
+  } else if (status === 'end') {
+    content = <End />;
+  }
+
+  return (
+    <div className={styles.wrap}>
+      <main className={styles.layout}>
+        <div className={styles.simulation_layout}>{content}</div>
+      </main>
+    </div>
+  );
+};
+
+export default Simulation;
--- src/app/interview/page.tsx
+++ src/app/interview/page.tsx
@@ -1,34 +1,11 @@
-'use client';
-
-import { useEffect, useState } from 'react';
 import styles from './page.module.scss';
-import { useRouter } from 'next/navigation';
-import { useUserInfo } from '@/queries/user/userApi';
-import { End, Ready, Start } from '@/widgets/interview';
+import Simulation from './Simulation';
 
 const Page = ({}) => {
-  const router = useRouter();
-  const [status, setStatus] = useState<'ready' | 'start' | 'end'>('ready');
-  const { data: isLogin, isFetched } = useUserInfo();
-
-  useEffect(() => {
-    if (isFetched && !isLogin) {
-      router.push('/signin');
-    }
-  }, [isFetched, isLogin, router]);
-
-  let content = <Ready onChangeStatus={setStatus} />;
-
-  if (status === 'start') {
-    content = <Start handleInterviewStatus={setStatus} />;
-  } else if (status === 'end') {
-    content = <End />;
-  }
-
   return (
     <div className={styles.wrap}>
       <main className={styles.layout}>
-        <div className={styles.simulation_layout}>{content}</div>
+        <Simulation />
       </main>
     </div>
   );
