3e17c4e97099670f636fc89bb81b0d382991cbfb
kuum97 <kuum97@icloud.com>
2024-09-30 16:04:40 +0900

    feat: 헤더 서버컴포넌트화 및 프로필이미지 쿠키에서 반영하여 하이드레이션 문제 해결

--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -1,24 +1,24 @@
-'use client';
-
-import styles from './Header.module.scss';
-import logo from '../../../public/Logo.png';
 import Image from 'next/image';
 import Link from 'next/link';
-import { useUserStore } from '@/shared/store/userStore';
-import { UserProfile } from '@/components/user';
+import { LogInBtn, UserProfile } from '@/components/user';
+import styles from './Header.module.scss';
+import { cookies } from 'next/headers';
 
 const Header = () => {
+  const cookieStore = cookies();
+  const profileImg = cookieStore.get('profileImg')?.value;
+
   return (
     <header className={styles.layout}>
       <nav className={styles.nav}>
         <Link href="/">
-          <li className={styles.logo}>
-            <Image src={logo} alt="logo" />
+          <div className={styles.logo}>
+            <Image src="/Logo.png" width={70} height={70} alt="logo" />
             <p>떨면뭐하니</p>
-          </li>
+          </div>
         </Link>
       </nav>
-      <UserProfile />
+      {profileImg ? <UserProfile profileImg={profileImg} /> : <LogInBtn />}
     </header>
   );
 };
