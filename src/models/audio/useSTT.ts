1e863deb17a5877af1b408d8c2aa456e1364a2fa
striker1826 <alstjq123579@daum.net>
2024-10-13 16:31:18 +0900

    feat: STT 기능 구현
    사용자의 음성을 Text로 바꾸는 STT 로직입니다.
    
    •	Web Speech API를 사용하여 음성 인식을 관리하는 커스텀 훅.
    •	이 훅은 오디오 녹음을 시작하고 중지하며, 음성 인식 결과를 텍스트로 캡처합니다.
    
    상세 내용은 코드의 JSDoc을 참고하시면 됩니다.

---
+++ src/models/audio/useSTT.ts
@@ -0,0 +1,45 @@
+'use client';
+
+import { useRef } from 'react';
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+
+/**
+
+	•	Web Speech API를 사용하여 음성 인식을 관리하는 커스텀 훅.
+	•	이 훅은 오디오 녹음을 시작하고 중지하며, 음성 인식 결과를 텍스트로 캡처합니다.
+	•	
+	•	@returns {Object} - 현재 인식된 텍스트와 녹음을 시작 및 중지하는 함수들을 반환합니다.
+	•	@property {React.MutableRefObject} text - 현재 인식된 텍스트를 저장하는 참조 객체입니다.
+	•	@property {Function} handleRecAudio - 오디오 입력을 녹음하고 인식하는 과정을 시작하는 함수입니다.
+	•	@property {Function} handleStopRecAudio - 녹음을 중지하고 인식된 텍스트를 저장하는 함수입니다.
+*/
+export const useSTT = () => {
+  const text = useRef<string>();
+  const { transcript, resetTranscript } = useSpeechRecognition();
+
+  /**
+   * 녹음을 시작하고 한국어로 음성을 인식합니다.
+   * 녹음 시작 전 인식된 텍스트를 빈 문자열로 초기화합니다.
+   * @returns {void}
+   */
+  const handleRecAudio = () => {
+    text.current = '';
+    SpeechRecognition.startListening({ continuous: true, language: 'ko' });
+  };
+
+  /**
+   * 현재 녹음을 중지하고, 인식된 텍스트를 저장합니다.
+   * 또한, 다음 녹음을 위해 인식된 텍스트를 초기화합니다.
+   */
+  const handleStopRecAudio = () => {
+    SpeechRecognition.abortListening();
+    text.current = transcript;
+    resetTranscript();
+  };
+
+  return {
+    text,
+    handleRecAudio,
+    handleStopRecAudio,
+  };
+};
