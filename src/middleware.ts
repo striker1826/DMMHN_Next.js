1aa2864ff0ca760c9ae2a39167a847d42aff45df
kuum97 <kuum97@icloud.com>
2024-10-21 20:05:17 +0900

    refactor: 가독성과 편의성을 위해 미들웨어 코드 개선

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,36 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { apiInstance } from '@/shared/utils/axios';
-import { protectRoute } from '@/shared/utils/protectRoute';
 
 export async function middleware(req: NextRequest) {
   const { pathname, searchParams } = req.nextUrl;
-  const code = searchParams.get('code');
+  const kakao_code = searchParams.get('code');
+  const accessToken = req.cookies.get('accessToken')?.value;
+  const oneDay = 24 * 60 * 60 * 1000;
+  const userAgent = req.headers.get('user-agent') || '';
+  const isMobile = /android|iphone|ipad|ipod|blackberry|iemobile|opera mini|mobile/i.test(
+    userAgent,
+  );
 
-  if (code) {
+  // 모바일로 접근 시 안내페이지로 리다이렉트 합니다.
+  if (isMobile) {
+    return NextResponse.redirect(new URL('/mobile-page', req.url));
+  }
+
+  // / 은 비보호 경로입니다.
+  if (pathname === '/') {
+    return NextResponse.next();
+  }
+
+  // 토큰 없이 보호 경로에 접근시 비보호 경로로 리다이렉트 합니다.
+  if (!accessToken) {
+    return NextResponse.redirect(new URL('/', req.url));
+  }
+
+  // 카카오 인증
+  if (kakao_code) {
     try {
       const response: unknown = await apiInstance.post('/auth/v2/kakao', {
-        code,
+        code: kakao_code,
       });
       const { access_token, user } = response as {
         access_token: string;
@@ -17,30 +38,14 @@ export async function middleware(req: NextRequest) {
       };
 
       const res = NextResponse.redirect(new URL('/', req.url));
-      res.cookies.set('accessToken', access_token);
-      res.cookies.set('profileImg', user.profileImg);
+      res.cookies.set('accessToken', access_token, { maxAge: oneDay });
+      res.cookies.set('profileImg', user.profileImg, { maxAge: oneDay });
 
       return res;
     } catch (error) {
       console.error('로그인 처리 중 오류 발생:', error);
       return NextResponse.redirect(new URL('/', req.url));
     }
-  }
-
-  const protectResponse = protectRoute({ req, route: 'interview' });
-  if (protectResponse) return protectResponse;
-
-  if (pathname === '/signin' || pathname === '/mobile-page') {
-    return NextResponse.next();
-  }
-
-  const userAgent = req.headers.get('user-agent') || '';
-  const isMobile = /android|iphone|ipad|ipod|blackberry|iemobile|opera mini|mobile/i.test(
-    userAgent,
-  );
-
-  if (isMobile) {
-    return NextResponse.redirect(new URL('/mobile-page', req.url));
   }
 
   return NextResponse.next();
