808105fb064e2b425b0a454c946ae4e69cf4bba6
kuum97 <kuum97@icloud.com>
2024-11-04 19:26:38 +0900

    style: Ready 상태 최종 수정

---
+++ src/components/interview/ReadyInfoCard.tsx
@@ -0,0 +1,33 @@
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  Flex,
+  Heading,
+  ListItem,
+  UnorderedList,
+} from '@chakra-ui/react';
+import { RiErrorWarningFill } from 'react-icons/ri';
+
+export default function ReadyInfoCard() {
+  return (
+    <Card variant="outline" border="1px" borderColor="blackAlpha.300" borderRadius="xl" maxW="md">
+      <CardHeader borderBottom="1px" borderColor="blackAlpha.300" padding="20px">
+        <Flex alignItems="center" gap="5px">
+          <RiErrorWarningFill fontSize="24px" />
+          <Heading fontSize="28px">중요합니다!</Heading>
+        </Flex>
+      </CardHeader>
+      <CardBody>
+        <UnorderedList fontSize="lg" fontWeight="md" spacing="2">
+          <ListItem>마이크를 충분히 가까이 하신 후 시작해주세요.</ListItem>
+          <ListItem>발음이 불분명하거나 빠르게 말씀하실 경우 인식이 어려울 수 있습니다.</ListItem>
+          <ListItem>답변을 완전히 말씀하신 후 1초 정도 뒤에 버튼을 눌러주세요.</ListItem>
+          <ListItem marginBottom="4px">
+            아래 버튼을 누르시고 마이크를 통해 목소리가 제대로 인식되는지 확인해주세요.
+          </ListItem>
+        </UnorderedList>
+      </CardBody>
+    </Card>
+  );
+}
--- src/widgets/interview/Ready/index.tsx
+++ src/widgets/interview/Ready/index.tsx
@@ -4,10 +4,11 @@ import 'regenerator-runtime/runtime';
 import { useRef } from 'react';
 import { useVideoHandler } from '@/models/simulation/video';
 import { useSTT } from '@/models/audio/useSTT';
-import { Button } from '@chakra-ui/react';
+import { Button, Flex } from '@chakra-ui/react';
 import { SlArrowLeft, SlArrowRight } from 'react-icons/sl';
-import { RiErrorWarningFill, RiMicFill, RiMicOffFill } from 'react-icons/ri';
+import { RiMicFill, RiMicOffFill } from 'react-icons/ri';
 import styles from './Ready.module.scss';
+import ReadyInfoCard from '@/components/interview/ReadyInfoCard';
 
 interface Props {
   transcript?: string;
@@ -42,29 +43,29 @@ export const Ready = ({ transcript, handleResetCurrentScript, onChangeStatus }: 
 
   return (
     <div className={styles.layout}>
-      <h1>카메라와 마이크를 준비해주세요!</h1>
+      <h1>준비 단계입니다! 마이크와 카메라를 확인해 주세요.</h1>
       <div className={styles.content_layout}>
         <div className={styles.video_wrap}>
           <video ref={videoRef} autoPlay playsInline muted />
         </div>
-        <div className={styles.description}>
-          <div className={styles.title}>
-            <RiErrorWarningFill />
-            <h2>중요합니다!</h2>
-          </div>
-          <ul className={styles.description_wrap}>
-            <li>마이크를 충분히 가까이 하신 후 시작해주세요.</li>
-            <li>발음이 불분명하거나 빠르게 말씀하실 경우 인식이 어려울 수 있습니다.</li>
-            <li>답변을 완전히 말씀하신 후 1초 정도 뒤에 버튼을 눌러주세요.</li>
-            <li>아래 버튼을 누르시고 마이크를 통해 목소리가 제대로 인식되는지 확인해주세요.</li>
-          </ul>
-          <div className={styles.stt_text_container}>
-            <Button onClick={handleAudio} variant="ghost" size="xs" fontSize="20px">
+        <Flex flexDirection="column" gap="20px">
+          <ReadyInfoCard />
+          <Flex
+            alignItems="center"
+            gap="5px"
+            padding="12px 8px"
+            borderRadius="xl"
+            fontSize="md"
+            fontWeight="md"
+            border="1px"
+            borderColor="blackAlpha.300"
+          >
+            <Button onClick={handleAudio} flexShrink="1" variant="ghost" size="xs" fontSize="md">
               {isListen ? <RiMicOffFill /> : <RiMicFill />}
             </Button>
             {transcript ? sliceTranscript(transcript) : '녹음된 음성이 Text로 표시됩니다!'}
-          </div>
-        </div>
+          </Flex>
+        </Flex>
       </div>
       <Button onClick={() => onChangeStatus('stacks')} variant="arrowLeft">
         <SlArrowLeft />
