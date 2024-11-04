46c60e2a118443800716b03bbcc5b1546134f3f2
kuum97 <kuum97@icloud.com>
2024-11-04 15:24:45 +0900

    style: 바디의 x축 padding을 줄여 위젯크기 개선

--- src/widgets/interview/Interviewing/index.tsx
+++ src/widgets/interview/Interviewing/index.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { useEffect, useRef } from 'react';
 import { useVideoHandler } from '@/models/simulation/video';
 import { useHandleInterview } from '@/models/simulation/useHandleInterview';
