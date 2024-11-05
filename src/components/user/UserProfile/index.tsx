a6ffb0fb4a082eb50246f54d2980903698b48d1a
kuum97 <kuum97@icloud.com>
2024-11-05 18:02:27 +0900

    fix: 불필요한 legacy prop 삭제

--- src/components/user/UserProfile/index.tsx
+++ src/components/user/UserProfile/index.tsx
@@ -46,14 +46,7 @@ export const UserProfileHamburger = ({ profileImg }: Props) => {
 export const UserProfile = ({ profileImg }: Props) => {
   return (
     <div className={styles.container}>
-      <Image
-        src={profileImg}
-        className={styles.profile_img}
-        width={50}
-        height={50}
-        alt="profile"
-        objectFit="fill"
-      />
+      <Image src={profileImg} className={styles.profile_img} width={50} height={50} alt="profile" />
     </div>
   );
 };
