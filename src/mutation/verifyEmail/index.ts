9bf3d14c5e21c5d388cc3b9f8d333cb5c5b6e2b2
kuum97 <kuum97@icloud.com>
2024-11-09 16:55:23 +0900

    feat: email verify 기능 구현

--- src/mutation/verifyEmail/index.ts
+++ src/mutation/verifyEmail/index.ts
@@ -2,15 +2,30 @@
 
 import { cookies } from 'next/headers';
 
-export async function verifyEmail(formData: FormData) {
-  const cookieStore = cookies();
-  const accessToken = cookieStore.get('accessToken')?.value;
+const cookieStore = cookies();
+const accessToken = cookieStore.get('accessToken')?.value;
 
-  const email = formData.get('email');
-
+export async function send(email: string) {
   const response = await fetch(`${process.env.BASE_URL}/auth/send/authcode`, {
     method: 'POST',
     body: JSON.stringify({ email }),
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error(`${response.status} ${response.statusText}`);
+  }
+
+  return true;
+}
+
+export async function verify({ email, code }: { email: string; code: string }) {
+  const response = await fetch(`${process.env.BASE_URL}/auth/confirm/authcode`, {
+    method: 'POST',
+    body: JSON.stringify({ email, code }),
     headers: {
       'Content-Type': 'application/json',
       Authorization: `Bearer ${accessToken}`,
