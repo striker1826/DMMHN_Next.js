6e05034f31523ebd74b9aca0fb6abaf08f890e01
kuum97 <kuum97@icloud.com>
2024-11-09 16:57:07 +0900

    feat: verify 코드 입력 폼 분리위해 코드 정리

--- src/components/auth/EmailVerification/index.tsx
+++ src/components/auth/EmailVerification/index.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { ChangeEvent, useEffect, useState, useTransition } from 'react';
+import { ChangeEvent, useState, useTransition } from 'react';
 import {
   Button,
   Flex,
@@ -9,24 +9,20 @@ import {
   FormLabel,
   Input,
   InputGroup,
-  InputRightElement,
-  Text,
 } from '@chakra-ui/react';
-import { verifyEmail } from '@/mutation/verifyEmail';
-import VerificationModal from '../VerificationModal';
+import { send } from '@/mutation/verifyEmail';
+import { useVerifyStore } from '@/shared/store/verifyStore';
 
 const EMAIL_REGEX =
   /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*\.[a-zA-Z]{2,}$/;
 
 interface Props {
-  setIsVerified: (value: boolean) => void;
+  setActiveStep: (step: number) => void;
 }
 
-export default function EmailVerification({ setIsVerified }: Props) {
-  const [email, setEmail] = useState<string>('');
+export default function EmailVerification({ setActiveStep }: Props) {
+  const { email, setEmail } = useVerifyStore();
   const [isError, setIsError] = useState<boolean>(false);
-  const [timer, setTimer] = useState<number | null>(null);
-  const [timeLeft, setTimeLeft] = useState<number>(180);
   const [isPending, startTransition] = useTransition();
 
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -36,21 +32,15 @@ export default function EmailVerification({ setIsVerified }: Props) {
     setIsError(!EMAIL_REGEX.test(email));
   };
 
-  const startTimer = () => {
-    setTimeLeft(180);
-    setTimer(
-      window.setInterval(() => {
-        setTimeLeft(prev => prev - 1);
-      }, 1000),
-    );
-  };
-
   const handleSubmit = async (formData: FormData) => {
+    const email = formData.get('email');
+
     startTransition(async () => {
       try {
-        const isVerified = await verifyEmail(formData);
-        if (isVerified) {
-          startTimer();
+        if (!email) return;
+        const isVerifyCode = await send(email.toString());
+        if (isVerifyCode) {
+          setActiveStep(+1);
         } else {
           setIsError(true);
         }
@@ -61,26 +51,19 @@ export default function EmailVerification({ setIsVerified }: Props) {
     });
   };
 
-  useEffect(() => {
-    if (timeLeft === 0 && timer) {
-      clearInterval(timer);
-      setTimer(null);
-      alert('타이머가 만료되었습니다. 다시 시도해 주세요.');
-    }
-  }, [timeLeft, timer]);
-
-  const reset = () => {
-    setEmail('');
-    setIsError(false);
-    setTimeLeft(180);
-  };
-
   return (
-    <Flex flexDirection="column" gap="5px" width="full" mt="10px" mb="20px">
-      <FormControl>
-        <form action={formData => handleSubmit(formData)}>
-          <FormLabel fontSize="lg" fontWeight="semibold">
-            조금만 더 진행해 주세요! <br />
+    <Flex flexDirection="column" gap="5px" width="full" height="full" mt="10px" mb="20px">
+      <form
+        action={formData => handleSubmit(formData)}
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'space-between',
+          height: '100%',
+        }}
+      >
+        <FormControl>
+          <FormLabel fontSize="xl" fontWeight="bold">
             간편 로그인 후 처음 한 번만 이메일 인증이 필요합니다. :{')'}
           </FormLabel>
           <InputGroup>
@@ -90,38 +73,27 @@ export default function EmailVerification({ setIsVerified }: Props) {
               value={email}
               placeholder="이메일 주소를 입력해 주세요."
               isInvalid={isError}
-              isDisabled={!!timer}
               onChange={handleInputChange}
               variant="flushed"
               borderColor="gray.400"
               pr="70px"
             />
-            <InputRightElement width="70px">
-              {timeLeft === 0 ? (
-                <Button onClick={reset} colorScheme="blue" size="sm">
-                  다시시도
-                </Button>
-              ) : (
-                <Button
-                  type="submit"
-                  isLoading={isPending}
-                  isDisabled={isError || email === '' || !!timer}
-                  colorScheme="green"
-                  size="sm"
-                >
-                  인증하기
-                </Button>
-              )}
-            </InputRightElement>
           </InputGroup>
-          {!timer && (
-            <FormHelperText textColor="gray.500">
-              입력하신 이메일은 인증 목적으로만 사용되며, 다른 용도로는 절대 활용되지 않습니다.
-              안심하고 입력해 주세요!
-            </FormHelperText>
-          )}
-        </form>
-      </FormControl>
+          <FormHelperText textColor="gray.500">
+            입력하신 이메일은 인증 목적으로만 사용되며, 다른 용도로는 절대 활용되지 않습니다.
+            안심하고 입력해 주세요!
+          </FormHelperText>
+        </FormControl>
+        <Button
+          type="submit"
+          isLoading={isPending}
+          isDisabled={isError || email === ''}
+          colorScheme="green"
+          size="lg"
+        >
+          인증하기
+        </Button>
+      </form>
     </Flex>
   );
 }
