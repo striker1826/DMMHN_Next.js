ad21de2c5acf37a986e1fa4440b44b5e373423b5
striker1826 <alstjq123579@daum.net>
2024-11-10 17:02:53 +0900

    fix: token이 만료된 상태에서 보호 경로로 이동했을 경우 home으로 리다이렉트

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,13 @@
 import { NextRequest, NextResponse, userAgent } from 'next/server';
 import { apiInstance } from '@/shared/utils/axios';
+import { cookies } from 'next/headers';
+import { AxiosError } from 'axios';
 
 const PROTECTED_PATHS = ['/interview'];
 const ONE_DAY_PER_SEC = 24 * 60 * 60;
 
 export async function middleware(req: NextRequest) {
+  const cookieStore = cookies();
   const { pathname, searchParams } = req.nextUrl;
 
   const kakao_code = searchParams.get('code');
@@ -67,6 +70,25 @@ export async function middleware(req: NextRequest) {
   // 보호 경로에 접근 시 토큰이 없는 경우 비보호 경로로 리다이렉트 합니다.
   if (PROTECTED_PATHS.includes(pathname) && !accessToken) {
     return NextResponse.redirect(new URL('/', req.url));
+  }
+
+  if (PROTECTED_PATHS.includes(pathname) && accessToken) {
+    console.log('accessToken', accessToken);
+    const res = await fetch(`${process.env.BASE_URL}/auth/login/check`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+    console.log('res', res.ok);
+    if (!res.ok) {
+      let res;
+      res = NextResponse.redirect(new URL('/', req.url));
+      res.cookies.delete('accessToken');
+      res.cookies.delete('profileImg');
+      return res;
+    }
   }
 
   return NextResponse.next();
